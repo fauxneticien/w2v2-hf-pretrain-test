7cf24a5baa6ef3abbf2f68d4a09daca73426fc23
Nay San <nay.san@stanford.edu>
2022-12-01 15:37:43 -0800

    Create get_xls-r_model.sh

---
+++ get_xls-r_model.sh
@@ -0,0 +1,10 @@
+OUTPUT_DIR="xls-r_300m"
+
+mkdir -p $OUTPUT_DIR
+pushd $OUTPUT_DIR
+# Get XLS-R model weights
+wget https://huggingface.co/facebook/wav2vec2-xls-r-300m/resolve/main/pytorch_model.bin
+# Use model config from w2v2 model known to work with pretraining script
+wget https://huggingface.co/patrickvonplaten/wav2vec2-large-repro-960h-libri-120k-steps/raw/main/config.json
+wget https://huggingface.co/patrickvonplaten/wav2vec2-large-repro-960h-libri-120k-steps/raw/main/preprocessor_config.json
+popd
